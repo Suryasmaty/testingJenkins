11e7035debdbc6710a15a0595d4d3af70a2dd37f
unknown <javamindc106@iiht.tech>
2022-01-16 13:34:11 +0530

    fir commit

---
+++ src/main/java/com/surya/AngularDemo1Application.java
@@ -0,0 +1,13 @@
+package com.surya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularDemo1Application.class, args);
+	}
+
+}
