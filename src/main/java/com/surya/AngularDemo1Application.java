28a522731198e459b698e22c5b956beae16bc641
unknown <javamindc106@iiht.tech>
2022-01-17 18:10:59 +0530

    tday commit

--- src/main/java/com/surya/AngularDemo1Application.java
+++ src/main/java/com/surya/AngularDemo1Application.java
@@ -8,6 +8,7 @@ public class AngularDemo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AngularDemo1Application.class, args);
+		System.out.println("hai");
 	}
 
 }
